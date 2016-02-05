4bba4d9a21b9daabe055f43ffdc9eb708bcef2e5
user.email <robert@penguinpower.co.nz>
2016-02-06 02:20:08 +1300

    added config object and some messages, changed imports

--- main/faye_server.go
+++ main/faye_server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/roncohen/faye-go"
-	"github.com/roncohen/faye-go/adapters"
+	"github.com/AutogrowSystems/faye-go"
+	"github.com/AutogrowSystems/faye-go/adapters"
 	"log"
 	"net/http"
 )
@@ -14,16 +14,28 @@ func OurLoggingHandler(pattern string, h http.Handler) http.Handler {
 	})
 }
 
+var cfg = struct {
+	Host   string
+	Port   string
+	Public string
+}{"127.0.0.1", "8000", "src/github.com/AutogrowSystems/faye-go/public"}
+
 func main() {
+	// TODO: read config file
+
 	fayeServer := faye.NewServer(faye.NewEngine())
 	http.Handle("/faye", adapters.FayeHandler(fayeServer))
 
-	// Also serve up some static files and show off 
+	// Also serve up some static files and show off
 	// the wonderful go http handler chain
-	http.Handle("/", OurLoggingHandler("/", http.FileServer(http.Dir("src/github.com/roncohen/faye-test/static"))))
+	http.Handle("/", OurLoggingHandler("/",
+		http.FileServer(http.Dir(cfg.Public)),
+	)) // TODO: put this in a config file
 
-	err := http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(cfg.Host+":"+cfg.Port, nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
+
+	fmt.Println("Faye server started on", cfg.Host+":"+cfg.Port)
 }
