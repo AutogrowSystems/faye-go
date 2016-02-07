5796395770def01bc99ec6aefb9c915c3b7569fe
user.email <robert@penguinpower.co.nz>
2016-02-08 11:54:57 +1300

    mutexed the message store

--- memory/msg_store.go
+++ memory/msg_store.go
@@ -3,23 +3,29 @@ package memory
 import (
 	"container/list"
 	"github.com/AutogrowSystems/faye-go/protocol"
+	"sync"
 )
 
 type MemoryMsgStore struct {
 	msgs *list.List
+	lock *sync.RWMutex
 }
 
 func NewMemoryMsgStore() *MemoryMsgStore {
-	return &MemoryMsgStore{list.New()}
+	return &MemoryMsgStore{list.New(), &sync.RWMutex{}}
 }
 
 func (m *MemoryMsgStore) EnqueueMessages(msgs []protocol.Message) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 	for _, msg := range msgs {
 		m.msgs.PushBack(msg)
 	}
 }
 
 func (m *MemoryMsgStore) GetAndClearMessages() []protocol.Message {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 	var msgArray = make([]protocol.Message, m.msgs.Len())
 	i := 0
 	for e := m.msgs.Front(); e != nil; e = e.Next() {
